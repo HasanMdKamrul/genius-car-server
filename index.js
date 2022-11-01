c523056b6865d767648792ae252b96a4baf9d03d
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-11-01 20:04:45 +0100

    db connected and the dotenv initialised

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 15000;
 const app = express();
+require('dotenv').config()
 
 // ** Middle Wares 
 
@@ -10,7 +12,22 @@ app.use(express.json());
 
 // ** initial server endpoint
 
-app.get('/', (req,res)=> res.send('genius car server is running'))
+app.get('/', (req,res)=> res.send('genius car server is running'));
+
+// ** DB COnnection
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.7ikallh.mongodb.net/?retryWrites=true&w=majority`;
+
+console.log(uri)
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
 
 
 // ** app listen
